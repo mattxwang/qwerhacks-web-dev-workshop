76e39e0a6171a240c6a709a13daf5a883413a31b
Matthew Wang <matt@matthewwang.me>
2019-11-30 22:48:26 -0800

    skeletoning out the react functionality
    basic create + update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      messages: [],
+      name: "J Bruin",
+      message: ""
+    };
+  }
+
+  handleNameChange = e => {
+    this.setState({ name: e.target.value });
+  }
+
+  handleMessageChange = e => {
+    this.setState({ message: e.target.value });
+  }
+
+  createMessage = () => {
+    let newMessage = {
+      name: this.state.name,
+      message: this.state.message
+    };
+    let newMessages = this.state.messages;
+    newMessages.push(newMessage)
+    this.setState({messages: newMessages});
+  }
+
+  renderMessages = () => {
+    if (this.state.messages.length === 0){
+      return (<div> it's empty. why not say something? </div>);
+    }
+    let messages = [];
+    this.state.messages.forEach((element, i) => {
+      messages.push(
+        <div key={i} className="message">
+          <p><span>{element.name}</span> said:</p>
+          <p>{element.message}</p>
+        </div>
+      );
+    });
+    return (<div className="messages-container">{messages}</div>);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="header-text">match</header>
+        <div className="message-box">
+          <input 
+            className="text-input"
+            value={this.state.Name} 
+            onChange={this.handleNameChange.bind(this)}
+            type="text"
+            placeholder="your name..."
+          />
+          <input 
+            className="text-input"
+            value={this.state.message} 
+            onChange={this.handleMessageChange.bind(this)}
+            type="text" 
+            placeholder="your message..." 
+          />
+          <button onClick={this.createMessage}>send</button>
+        </div>
+        <hr />
+        {this.renderMessages()}
+      </div>
+    );
+  }
 }
 
 export default App;
